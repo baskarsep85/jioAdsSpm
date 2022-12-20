debd160e5571c74d45f137dd80c0492a7e2deffb
Prasad Kandukuri <prasad.kandukuri@ril.com>
2022-12-20 15:35:58 +0530

    Test

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "jioAdsSpm",
+    platforms: [.tvOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+//        .library(
+//            name: "jioAdsSpm",
+//            targets: ["jioAdsSpm"]),
         .library(
             name: "jioAdsSpm",
-            targets: ["jioAdsSpm"]),
+            targets: ["JioAdsFrameworkTVOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +22,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "jioAdsSpm",
-            dependencies: []),
-        .testTarget(
-            name: "jioAdsSpmTests",
-            dependencies: ["jioAdsSpm"]),
+        .binaryTarget(name: "JioAdsFrameworkTVOS", path: "./Sources/JioAdsFrameworkTVOS.xcframework"),
+//        .target(
+//            name: "jioAdsSpm",
+//            dependencies: []),
     ]
 )
